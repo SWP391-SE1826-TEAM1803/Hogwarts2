3296872263e7a2fc30448ac9b36c5a89c84afb2d
nnt7923 <nntruong79230@gmail.com>
2024-07-18 10:27:19 +0700

    d

--- src/java/model/DBConnect.java
+++ src/java/model/DBConnect.java
@@ -28,8 +28,8 @@ public class DBConnect {
     }
 
     public DBConnect() {
-        this("jdbc:sqlserver://localhost:1433;databaseName=hw11",
-                "sa","123456");
+        this("jdbc:sqlserver://localhost:1433;databaseName=Hogwart",
+                "sa","12345678");
 
     }
 
